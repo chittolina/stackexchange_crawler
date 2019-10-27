27e5c59739408f21b387ff52a50a0d6a0279355e
Gabriel Chittolina <gabrielchittolina1@gmail.com>
2019-10-27 19:22:42 -0300

    Add initial functions to list/save users on database

--- lib/stackexchange_crawler.ex
+++ lib/stackexchange_crawler.ex
@@ -1,9 +1,31 @@
 defmodule StackexchangeCrawler do
   @moduledoc """
-  StackexchangeCrawler keeps the contexts that define your domain
-  and business logic.
-
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
+  StackexchangeCrawler contains resources for contacting the StackExchange API for users listing
   """
+  use Tesla
+
+  plug(Tesla.Middleware.BaseUrl, "https://api.stackexchange.com/2.2")
+
+  def get_users() do
+    {:ok, response} = get("/users?site=stackoverflow")
+
+    :zlib.gunzip(response.body)
+    |> Jason.decode!()
+    |> Map.get("items")
+  end
+
+  def get_and_insert_users() do
+    users =
+      get_users()
+      |> Enum.map(fn user ->
+        %{
+          user_id: user["user_id"],
+          display_name: user["display_name"],
+          profile_image: user["profile_image"],
+          location: user["location"]
+        }
+      end)
+
+    StackexchangeCrawler.Repo.insert_all(StackexchangeCrawler.User, users)
+  end
 end
--- mix.exs
+++ mix.exs
@@ -42,7 +42,10 @@ defmodule StackexchangeCrawler.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:tesla, "~> 1.2.0"},
+      # optional, but recommended adapter
+      {:hackney, "~> 1.14.0"}
     ]
   end
 
